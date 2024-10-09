890dc6c81123014e5bb3fd4080cb0fbb55e573a0
InkyChew <inky11308@gmail.com>
2024-10-09 17:31:02 +0800

    Schedule work

---
+++ todolist_be/todolist_be/Services/ScheduleHostedService.cs
@@ -0,0 +1,56 @@
+﻿using Serilog;
+
+namespace todolist_be.Services
+{
+    public class ScheduleHostedService
+    {
+        private readonly IServiceProvider _services;
+        private Timer? _dailyTimer = null;
+
+        public ScheduleHostedService(IServiceProvider services)
+        {
+            _services = services;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Log.Information("ScheduleHostedService start.");
+            DateTime now = DateTime.Now;
+            ScheduleDaily(now);
+            return Task.CompletedTask;
+        }
+
+        public void ScheduleDaily(DateTime now)
+        {
+            DateTime nextRunTime = new(now.Year, now.Month, now.Day);
+
+            if (now > nextRunTime) nextRunTime = nextRunTime.AddDays(1);
+
+            TimeSpan timeToGo = nextRunTime - now;
+
+            _dailyTimer = new Timer(RunDailyTask, null, timeToGo, TimeSpan.FromDays(1));
+        }
+        public void RunDailyTask(object? state)
+        {
+            var now = DateTime.Now;
+            Log.Information($"Run daily task at {now}");
+
+            IServiceScope _scope = _services.CreateScope();
+            var service = _scope.ServiceProvider.GetRequiredService<IScheduleService>();
+            service.DailyHandler(now);
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _dailyTimer?.Change(Timeout.Infinite, 0);
+            Log.Information("ScheduleHostedService stop.");
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _dailyTimer?.Dispose();
+            GC.SuppressFinalize(this);
+            Log.Information("ScheduleHostedService dispose.");
+        }
+    }
+}
---
+++ todolist_be/todolist_be/Services/ScheduleService.cs
@@ -0,0 +1,55 @@
+﻿using Serilog;
+using todolist_be.Models;
+
+namespace todolist_be.Services
+{
+    public interface IScheduleService
+    {
+        public Task DailyHandler(DateTime now);
+    }
+    public class ScheduleService : IScheduleService
+    {
+        private readonly IWorkService _workService;
+
+        public ScheduleService(IWorkService workService)
+        {
+            _workService = workService;
+        }
+
+        public async Task DailyHandler(DateTime now)
+        {
+            await CreateWorkWhenDateReachAsync(now);
+        }
+        public async Task CreateWorkWhenDateReachAsync(DateTime now)
+        {
+            try
+            {
+                WorkSearchParam taskParam = new() { StartDate = now, EndDate = now };
+                var reachWorks = (await _workService.GetAllWorkAsync(taskParam)).Where(_ => _.Period != Period.None);
+
+                if (reachWorks == null || !reachWorks.Any())
+                {
+                    Log.Information("No reach works to create.");
+                    return;
+                }
+
+                foreach (var work in reachWorks)
+                {
+                    try
+                    {
+                        var nextWork = _workService.CreateNextWork(work);
+                        await _workService.CreateWorkAsync(nextWork);
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error(ex, $"An error occurred while create next work with Id: {work.Id}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred in CreateWorkWhenDateReachAsync");
+            }
+        }
+    }
+}
